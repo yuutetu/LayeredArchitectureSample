f8361a3c3dcb717fe94348f3600f618852be20e9
yuutetu <yuutetu@gmail.com>
2019-03-24 19:36:26 +0900

    first commit

---
+++ PaginatableList/PaginatableList/paginatable_list/infrastructure/network/PaginatableListAPI.swift
@@ -0,0 +1,17 @@
+//
+//  PaginatableListAPI.swift
+//  PaginatableList
+//
+//  Created by 加賀江 優幸 on 2019/03/24.
+//  Copyright © 2019 yuutetu. All rights reserved.
+//
+
+class PaginatableListAPI {
+    init() {
+
+    }
+
+    func request() {
+
+    }
+}
